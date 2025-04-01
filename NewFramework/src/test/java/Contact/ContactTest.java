79e7b5c2387db0472df0dea59b9966664e06a0be
503360078 <503360078@HC-CBKTDK3>
2025-04-01 09:35:57 +0530

    FrameworkPush_1

---
+++ NewFramework/src/test/java/Contact/ContactTest.java
@@ -0,0 +1,12 @@
+package Contact;
+
+public class ContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("New contact is created");
+
+	}
+
+}
---
+++ NewFramework/src/test/java/Contact/LeadsTest.java
@@ -0,0 +1,12 @@
+package Contact;
+
+public class LeadsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("New leads is created");
+
+	}
+
+}
